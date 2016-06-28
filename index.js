36094d510919d0bd97bbbd899f7aad951518f862
Hitesh Khandelwal <hitesh@smartprix.com>
2016-06-28 10:52:09 +0530

    modify according to sm-utils

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ module.exports = {
     'object-curly-spacing': ['error', 'never'],
     'prefer-template': 0,
     'no-cond-assign': ['error', 'except-parens'],
+    'no-underscore-dangle': 0,
+
     'babel/generator-star-spacing': 1,
     'babel/no-await-in-loop': 1,
-    'no-underscore-dangle': 0,
+
+    'babel/array-bracket-spacing': 0,
+    'babel/arrow-parens': 0,
+    'babel/flow-object-type': 0,
+    'babel/func-params-comma-dangle': 0,
+    'babel/new-cap': 0,
+    'babel/object-curly-spacing': 0,
+    'babel/object-shorthand': 0,
   }
 };
