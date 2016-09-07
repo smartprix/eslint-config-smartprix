05106fc055d1d8b51d994aec08e03e2188943433
Hitesh Khandelwal <hitesh@smartprix.com>
2016-09-07 08:17:36 +0530

    extend airbnb instead of duplicating it. add warnings for complex code

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
 module.exports = {
-  extends: [
-    './legacy',
-    './rules/es6',
-    './rules/imports',
-  ].map(require.resolve),
-  parser: 'babel-eslint',
-  plugins: [
-    'babel'
-  ],
-  parserOptions: {
-    ecmaVersion: 7,
-    sourceType: 'module',
-  },
-  rules: {
-    strict: 2,
-    'generator-star-spacing': 0,
-    'func-names': 0,
-    'no-param-reassign': 0,
-    'object-curly-spacing': ['error', 'never'],
-    'prefer-template': 0,
-    'no-cond-assign': ['error', 'except-parens'],
-    'no-underscore-dangle': 0,
+	extends: [
+		'eslint-config-airbnb-base'
+	].map(require.resolve),
 
-    'babel/generator-star-spacing': 1,
-    'babel/no-await-in-loop': 1,
+	parser: 'babel-eslint',
+	plugins: [
+		'babel'
+	],
 
-    'babel/array-bracket-spacing': 0,
-    'babel/arrow-parens': 0,
-    'babel/flow-object-type': 0,
-    'babel/func-params-comma-dangle': 0,
-    'babel/new-cap': 0,
-    'babel/object-curly-spacing': 0,
-    'babel/object-shorthand': 0,
-  }
+	parserOptions: {
+		ecmaVersion: 2016,
+		sourceType: 'module',
+	},
+	
+	rules: {
+		'brace-style': [2, 'stroustrup', { allowSingleLine: true }],
+		'indent': [2, 'tab', { SwitchCase: 1, VariableDeclarator: 1 }],
+
+		'generator-star-spacing': 0,
+		'func-names': 0,
+		'no-param-reassign': 0,
+		'object-curly-spacing': ['error', 'never'],
+		'prefer-template': 0,
+		'no-cond-assign': ['error', 'except-parens'],
+		'no-underscore-dangle': 0,
+		'no-plusplus': 0,
+		'operator-linebreak': ['error', 'after'],
+
+		'max-nested-callbacks': ['warn', 3],
+		'max-depth': ['warn', 4],
+		'max-statements': ['warn', 40],
+		'max-params': ['warn', 5],
+		'complexity': ['warn', 20],
+		'consistent-this': ['warn', 'self'],
+		'max-lines': ['warn', {
+			max: 300,
+			skipBlankLines: true,
+			skipComments: true
+		}],
+		'max-statements-per-line': ['warn', { max: 2 }],
+		//'no-negated-condition': 'warn',
+
+		'babel/generator-star-spacing': ['error', {'before': false, 'after': true}],
+		'babel/no-await-in-loop': 1,
+
+		'babel/array-bracket-spacing': 0,
+		'babel/arrow-parens': 0,
+		'babel/flow-object-type': 0,
+		'babel/func-params-comma-dangle': 0,
+		'babel/new-cap': 0,
+		'babel/object-curly-spacing': 0,
+		'babel/object-shorthand': 0,
+	}
 };
