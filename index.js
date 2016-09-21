f652f26657a1fb140fcc1559976f1f33e4570fd9
Hitesh Khandelwal <hitesh@smartprix.com>
2016-09-21 10:13:27 +0530

    remove unresolved rule

--- index.js
+++ index.js
@@ -68,9 +68,8 @@ module.exports = {
 
 		'import/max-dependencies': 0,
 		'import/no-absolute-path': 0,
-	},
-
-	settings: {
-		'import/extensions': ['.js', '.jsx', '.json', '.vue'],
+		
+		// doesn't work properly with webpack
+		'import/no-unresolved': 0,
 	},
 };
