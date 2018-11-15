24b4c0659a8a01d8c69a527247bc2054aa5efe0a
Rohit Gohri <rohit.gohri@smartprix.com>
2018-11-15 17:58:12 +0530

    lint files

--- test/test-base.js
+++ test/test-base.js
@@ -4,26 +4,26 @@ import test from 'tape';
 
 const index = require('../');
 
-const files = { index };
+const files = {index};
 
-fs.readdirSync(path.join(__dirname, '../rules')).forEach(name => {
-  files[name] = require(`../rules/${name}`); // eslint-disable-line global-require
+fs.readdirSync(path.join(__dirname, '../rules')).forEach((name) => {
+	files[name] = require(`../rules/${name}`); // eslint-disable-line global-require, import/no-dynamic-require
 });
 
-Object.keys(files).forEach(name => {
-  const config = files[name];
+Object.keys(files).forEach((name) => {
+	const config = files[name];
 
-  test(`${name}: does not reference react`, t => {
-    t.plan(2);
+	test(`${name}: does not reference react`, (t) => {
+		t.plan(2);
 
-    // scan plugins for react and fail if it is found
-    const hasReactPlugin = Object.prototype.hasOwnProperty.call(config, 'plugins') &&
-      config.plugins.indexOf('react') !== -1;
-    t.notOk(hasReactPlugin, 'there is no react plugin');
+		// scan plugins for react and fail if it is found
+		const hasReactPlugin = Object.prototype.hasOwnProperty.call(config, 'plugins') &&
+			config.plugins.indexOf('react') !== -1;
+		t.notOk(hasReactPlugin, 'there is no react plugin');
 
-    // scan rules for react/ and fail if any exist
-    const reactRuleIds = Object.keys(config.rules)
-      .filter(ruleId => ruleId.indexOf('react/') === 0);
-    t.deepEquals(reactRuleIds, [], 'there are no react/ rules');
-  });
+		// scan rules for react/ and fail if any exist
+		const reactRuleIds = Object.keys(config.rules)
+			.filter(ruleId => ruleId.indexOf('react/') === 0);
+		t.deepEquals(reactRuleIds, [], 'there are no react/ rules');
+	});
 });
